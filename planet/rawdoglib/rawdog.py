669b820f013aa3390124a23766ac9d23e6ad2c94
akshanshbhatt <qaz.akshansh@gmail.com>
2021-05-26 13:35:26 +0530

    encoding changes

--- planet/rawdoglib/rawdog.py
+++ planet/rawdoglib/rawdog.py
@@ -942,7 +942,7 @@ class Config:
 			f = open(filename, "r")
 			for line in f:
 				try:
-					line = line.decode(get_system_encoding())
+					line =  str(line, get_system_encoding()) # line.decode(get_system_encoding())
 				except UnicodeDecodeError as e:
 					raise ConfigError("Character encoding problem in config file: " + filename + ": " + str(e))
 
